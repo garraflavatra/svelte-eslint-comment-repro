0be5825aace6a62bc42b27db398f3c2eef0b6384
Romein van Buren <romein@vburen.nl>
2023-08-04 19:39:54 +0200

    Comment indentation bug reproduction

---
+++ expected.svelte
@@ -0,0 +1,28 @@
+<script>
+
+  function demo1() {
+    // This should be indented with 4 spaces
+    console.log('ok');
+  }
+
+  function demo2() {
+    // This should be indented with 4 spaces
+  }
+
+  function demo3() {
+    // This should be indented with 4 spaces
+    // Another line
+    console.log('ok');
+  }
+
+  function demo4() {
+    // This should be indented with 4 spaces
+    // Another line
+  }
+
+  demo1();
+  demo2();
+  demo3();
+  demo4();
+
+</script>
